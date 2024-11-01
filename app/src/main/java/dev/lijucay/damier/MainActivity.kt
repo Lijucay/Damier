61c980c6d12931ac02a260ea1785d61169087fa2
Lijucay (Luca) <lico.keins@gmail.com>
2024-11-01 10:42:13 +0100

    Created a variable that observes whether or not to show the import dialog Created variable that observes the user's default goal preference Created variable to observe the current habit title as a replacement for the variable, that observed the habit's ID Created variable to observe whether or not to show the check in dialog Created variable to observe the current file uri Added logic for the case, the check-in dialog needs to be shown Replaced references to the habit's ID with references to the habit's title Added logic for the case that the import dialog has to be shown

--- app/src/main/java/dev/lijucay/damier/MainActivity.kt
+++ app/src/main/java/dev/lijucay/damier/MainActivity.kt
@@ -17,7 +17,9 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 import dev.lijucay.damier.data.local.model.TrackingInfo
 import dev.lijucay.damier.presentation.dialogs.AddHabitDialog
+import dev.lijucay.damier.presentation.dialogs.CheckIns
 import dev.lijucay.damier.presentation.dialogs.EditHabitDialog
+import dev.lijucay.damier.presentation.dialogs.ImportDialog
 import dev.lijucay.damier.presentation.dialogs.IncrementCountDialog
 import dev.lijucay.damier.presentation.dialogs.InfoDialog
 import dev.lijucay.damier.presentation.screens.MainScreen
@@ -44,16 +46,24 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         habitViewModel.loadHabits()
 
         setContent {
             DamierTheme {
+                val scope = rememberCoroutineScope()
+                val context = LocalContext.current
                 val habitList by habitViewModel.habitList.collectAsStateWithLifecycle()
+                val showImportDialog by uiViewModel.showImportDialog.collectAsStateWithLifecycle()
+
                 trackingInfoViewModel.collectTrackingInfo(habitList)
                 trackingInfoViewModel.collectWaffleBoardInfo(habitList)
+                val defaultGoal by context.dataStore.data
+                    .map { preferences -> preferences[DEFAULT_GOAL] ?: 10 }
+                    .collectAsStateWithLifecycle(10)
 
-                val currentHabitID by habitViewModel.currentSelectedHabit.collectAsStateWithLifecycle()
+                MainScreen(uiViewModel, habitViewModel, trackingInfoViewModel)
+
+                val currentHabitTitle by habitViewModel.currentSelectedHabit.collectAsStateWithLifecycle()
 
                 val showCounterDialog by uiViewModel.showCounterDialog.collectAsStateWithLifecycle()
                 val showAddHabitDialog by uiViewModel.showAddHabitDialog.collectAsStateWithLifecycle()
@@ -61,24 +71,38 @@ class MainActivity : ComponentActivity() {
                 val showInfoDialog by uiViewModel.showInfoDialog.collectAsStateWithLifecycle()
                 val showEditHabitDialog by uiViewModel.showEditHabitDialog.collectAsStateWithLifecycle()
                 val showEditDefaultGoalDialog by uiViewModel.showEditDefaultGoalDialog.collectAsStateWithLifecycle()
+                val showCheckIns by uiViewModel.showCheckIns.collectAsStateWithLifecycle()
 
-                val scope = rememberCoroutineScope()
-                val context = LocalContext.current
+                val currentFileUri by uiViewModel.currentFileUri.collectAsStateWithLifecycle()
 
-                val defaultGoal by context.dataStore.data
-                    .map { preferences -> preferences[DEFAULT_GOAL] ?: 10 }
-                    .collectAsStateWithLifecycle(10)
+                if (showCheckIns) {
+                    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
 
-                MainScreen(uiViewModel, habitViewModel, trackingInfoViewModel)
+                    currentHabitTitle?.let { title ->
+                        val currentHabit = habitList.find { it.title == title }
+
+                        currentHabit?.let {
+                            CheckIns(
+                                sheetState = sheetState,
+                                habit = it,
+                                trackingInfoViewModel = trackingInfoViewModel
+                            ) {
+                                hideSheet(scope, sheetState) {
+                                    uiViewModel.setShowCheckIns(false)
+                                }
+                            }
+                        }
+                    }
+                }
 
                 if (showCounterDialog) {
                     val sheetState = rememberModalBottomSheetState()
                     val dateFormatter = DateTimeFormatter.ISO_LOCAL_DATE
                     val timeFormatter = DateTimeFormatter.ISO_LOCAL_TIME
 
-                    currentHabitID?.let { id ->
+                    currentHabitTitle?.let { title ->
                         val currentHabit = habitList.find { habit ->
-                            habit.id == id
+                            habit.title == title
                         }
 
                         currentHabit?.let {
@@ -89,7 +113,7 @@ class MainActivity : ComponentActivity() {
                                         uiViewModel.setShowCounterDialog(false)
                                         trackingInfoViewModel.insertTrackingInfo(
                                             TrackingInfo(
-                                                habitId = id,
+                                                habitTitle = title,
                                                 date = LocalDate.now().format(dateFormatter),
                                                 time = LocalTime.now().format(timeFormatter),
                                                 count = count
@@ -133,8 +157,8 @@ class MainActivity : ComponentActivity() {
                     InfoDialog(uiViewModel) { uiViewModel.setInfoDialogInfo(show = false) }
 
                 if (showEditHabitDialog) {
-                    currentHabitID?.let { id ->
-                        val habit = habitList.find { it.id == id }
+                    currentHabitTitle?.let { title ->
+                        val habit = habitList.find { it.title == title }
                         habit?.let {
                             val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
                             EditHabitDialog(
@@ -176,6 +200,20 @@ class MainActivity : ComponentActivity() {
                         hideSheet(scope, sheetState) {
                             uiViewModel.setShowEditDefaultGoalDialog(false)
                         }
+                    }
+                }
+
+                if (showImportDialog) {
+                    currentFileUri?.let {
+                        ImportDialog(
+                            onDismiss = { },
+                            onComplete = {
+                                uiViewModel.setShowImportDialog(false)
+                                uiViewModel.setCurrentFileUri(null)
+                            },
+                            uiViewModel = uiViewModel,
+                            fileUri = it
+                        )
                     }
                 }
             }
