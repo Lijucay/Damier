092f9d8b8531d9957690aaf711dd894d7d833757
Lijucay (Luca) <lico.keins@gmail.com>
2024-11-01 10:31:12 +0100

    Changing versionName and versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.lijucay.damier"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0 alpha"
+        versionCode = 2
+        versionName = "0.2 alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -34,11 +34,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
