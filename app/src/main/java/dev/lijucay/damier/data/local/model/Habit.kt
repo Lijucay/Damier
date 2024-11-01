ccd1de7deb786b9ce723247bd510e06842bfdb14
Lijucay (Luca) <lico.keins@gmail.com>
2024-11-01 10:32:31 +0100

    Remove auto generated primary key (id), make title the primary key of habits

--- app/src/main/java/dev/lijucay/damier/data/local/model/Habit.kt
+++ app/src/main/java/dev/lijucay/damier/data/local/model/Habit.kt
@@ -1,17 +1,11 @@
 package dev.lijucay.damier.data.local.model
 
 import androidx.room.Entity
-import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(
-    indices = [
-        Index(value = ["title"], unique = true)
-    ]
-)
+@Entity
 data class Habit(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val title: String,
+    @PrimaryKey(autoGenerate = false) val title: String,
     val description: String? = null,
     val singularUnitName: String = "",
     val pluralUnitName: String = "",
