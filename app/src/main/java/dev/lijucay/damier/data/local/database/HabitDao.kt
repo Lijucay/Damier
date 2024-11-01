4ce4e0985ffc2f2e561738ab4d0ffe7c5d654fc8
Lijucay (Luca) <lico.keins@gmail.com>
2024-11-01 10:33:19 +0100

    Remove functions that got a habit based on the habit's id and replaced them with functions that get a habit based on its title

--- app/src/main/java/dev/lijucay/damier/data/local/database/HabitDao.kt
+++ app/src/main/java/dev/lijucay/damier/data/local/database/HabitDao.kt
@@ -1,7 +1,6 @@
 package dev.lijucay.damier.data.local.database
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -14,17 +13,20 @@ interface HabitDao {
     @Query("select * from Habit")
     fun getHabits(): Flow<List<Habit>>
 
-    @Query("select * from Habit where id = :id")
-    suspend fun getHabitById(id: Int): Habit?
+    @Query("select * from Habit")
+    fun getCurrentHabits():List<Habit>
+
+    @Query("select * from Habit where title = :title")
+    suspend fun getHabitByTitle(title: String): Habit?
 
     @Insert(Habit::class, OnConflictStrategy.IGNORE)
     suspend fun insertHabit(habit: Habit): Long
 
-    @Query("select * from Habit where id = :id")
-    fun getCurrentHabitById(id: Int): Flow<Habit?>
+    @Query("select * from Habit where title = :title")
+    fun getCurrentHabitByTitle(title: String): Flow<Habit?>
 
-    @Query("delete from Habit where id = :id")
-    suspend fun deleteHabit(id: Int): Int
+    @Query("delete from Habit where title = :title")
+    suspend fun deleteHabit(title: String): Int
 
     @Update
     suspend fun updateHabit(habit: Habit): Int
