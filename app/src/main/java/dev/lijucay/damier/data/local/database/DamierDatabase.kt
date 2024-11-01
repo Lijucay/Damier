a2a1cb843a5ba57f1d9a33754db27ccc1186d78e
Lijucay (Luca) <lico.keins@gmail.com>
2024-11-01 10:31:52 +0100

    Use a constant variable that can be shared throughout the application's code for the version of the database

--- app/src/main/java/dev/lijucay/damier/data/local/database/DamierDatabase.kt
+++ app/src/main/java/dev/lijucay/damier/data/local/database/DamierDatabase.kt
@@ -4,10 +4,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import dev.lijucay.damier.data.local.model.Habit
 import dev.lijucay.damier.data.local.model.TrackingInfo
+import dev.lijucay.damier.util.Utils.DATABASE_SCHEME_VERSION
 
 @Database(
     entities = [Habit::class, TrackingInfo::class],
-    version = 1,
+    version = DATABASE_SCHEME_VERSION,
     exportSchema = false
 )
 abstract class DamierDatabase : RoomDatabase() {
