64c1e95362e5b4c6d2d49d15bd94f85892694dad
Lijucay (Luca) <lico.keins@gmail.com>
2024-11-01 10:47:33 +0100

    Changing the foreign key of TrackingInfo to the habit title, replacing habit id Removed auto generated primary key (id) and made a combination of habitTitle, date and time the new key

--- app/src/main/java/dev/lijucay/damier/data/local/model/TrackingInfo.kt
+++ app/src/main/java/dev/lijucay/damier/data/local/model/TrackingInfo.kt
@@ -3,21 +3,23 @@ package dev.lijucay.damier.data.local.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(
+    primaryKeys = ["habitTitle", "date", "time"],
     foreignKeys = [
         ForeignKey(
             entity = Habit::class,
-            parentColumns = ["id"],
-            childColumns = ["habitId"],
+            parentColumns = ["title"],
+            childColumns = ["habitTitle"],
             onDelete = ForeignKey.CASCADE
         )
-    ]
+    ],
+    indices = [Index(value = ["habitTitle"])]
 )
 data class TrackingInfo(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val habitId: Int,
+    val habitTitle: String,
     val date: String,
     val time: String,
     @ColumnInfo(defaultValue = "0") val count: Int
