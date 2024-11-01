708474c6ef3443b5d2a9729c9057ab0ebc27e014
Lijucay (Luca) <lico.keins@gmail.com>
2024-11-01 10:37:33 +0100

    Replaced habit-loading-functions using the habit's ID with function that use the habit's title

--- app/src/main/java/dev/lijucay/damier/data/local/repository/HabitRepository.kt
+++ app/src/main/java/dev/lijucay/damier/data/local/repository/HabitRepository.kt
@@ -6,8 +6,8 @@ import kotlinx.coroutines.flow.Flow
 interface HabitRepository {
     fun loadHabits(): Flow<List<Habit>>
     suspend fun insertHabit(habit: Habit): Long
-    suspend fun getHabitByID(id: Int): Habit?
-    fun getCurrentHabitByID(id: Int): Flow<Habit?>
-    suspend fun deleteHabit(id: Int): Int
+    suspend fun getHabitByTitle(title: String): Habit?
+    fun getCurrentHabitByTitle(title: String): Flow<Habit?>
+    suspend fun deleteHabit(title: String): Int
     suspend fun updateHabit(habit: Habit): Int
 }
